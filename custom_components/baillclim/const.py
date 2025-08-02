b3159deb9e0bb9f57ac0613de9fa1d22d8391a36
hebrru <134215992+hebrru@users.noreply.github.com>
2025-08-02 16:24:07 +0200

    Update const.py

--- custom_components/baillclim/const.py
+++ custom_components/baillclim/const.py
@@ -1,5 +1,5 @@
 
 DOMAIN = "baillclim"
 LOGIN_URL = "https://www.baillconnect.com/client/connexion"
-REGULATIONS_URL = "https://www.baillconnect.com/client/regulations/270"
-COMMAND_URL = "https://www.baillconnect.com/api-client/regulations/270"
+REGULATIONS_URL = "https://www.baillconnect.com/client/regulations/XXX"
+COMMAND_URL = "https://www.baillconnect.com/api-client/regulations/XXX"
