e64e0be383f6ebc41db0b1e9046eb921634b0f84
hebrru <134215992+hebrru@users.noreply.github.com>
2025-08-03 18:55:45 +0200

    correction de bug

---
+++ custom_components/baillclim/const.py
@@ -0,0 +1,5 @@
+
+DOMAIN = "baillclim"
+LOGIN_URL = "https://www.baillconnect.com/client/connexion"
+REGULATIONS_URL = "https://www.baillconnect.com/client/regulations/270"
+COMMAND_URL = "https://www.baillconnect.com/api-client/regulations/270"
