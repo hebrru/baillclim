21ea4d5fb5defed31f63dc40b69516e828f818dc
hebrru <134215992+hebrru@users.noreply.github.com>
2025-08-03 18:54:25 +0200

    Delete custom_components/baillclim/const.py

--- custom_components/baillclim/const.py
+++
@@ -1,5 +0,0 @@
-
-DOMAIN = "baillclim"
-LOGIN_URL = "https://www.baillconnect.com/client/connexion"
-REGULATIONS_URL = "https://www.baillconnect.com/client/regulations/270"
-COMMAND_URL = "https://www.baillconnect.com/api-client/regulations/270"
